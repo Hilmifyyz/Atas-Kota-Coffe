31a3418daede1e9a152320afb283206091db3b6b
Noparu <94230308+Ihsannaufal01@users.noreply.github.com>
2024-11-23 20:15:46 +0700

    Hero section
    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar'
-import Layout from './components/Layout'
 import Home from './pages/home';
 
 function App() {
 
   return (
     <Router>
-      {/* <Layout> */}
         <Navbar/>
         <Home/>
-      {/* </Layout> */}
-
     </Router>
   )
 }
