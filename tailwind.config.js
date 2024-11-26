7214038c23e6053d2d6653ebfd7813162c825eeb
Noparu <94230308+Ihsannaufal01@users.noreply.github.com>
2024-11-26 09:11:50 +0700

    Horizontal Scroll
    scroll Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,22 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'Logo': "url('/src/assets/Logo.png')",
-        'LogoMini': "url('/src/assets/Logo Mini.png')",
-        'Coffee': "url('/src/assets/Coffee.png')",
+        'Logo': "url('/src/assets/Photos/Logo.png')",
+        'LogoMini': "url('/src/assets/Photos/Logo Mini.png')",
+        'Coffee': "url('/src/assets/Photos/Coffee.png')",
+        'Alfredo': "url('/src/assets/Photos/Alfredo Pasta.png')",
+        'CaffeLatte': "url('/src/assets/Photos/Caffe Latte.png')",
+        'ChickenPasta': "url('/src/assets/Photos/Chicken Pasta.png')",
+        'Espresso': "url('/src/assets/Photos/Espresso.png')",
+        'RotiBakar': "url('/src/assets/Photos/Roti Bakar.png')",
       }, 
       colors: {
         'brownpage': '#FFFBF2',
         'brownbutton': '#CBBDA6',
         'brownbuttonhover': '#B0A392',
+      },
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
       },
     },
   },
