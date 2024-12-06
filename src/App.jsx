78e758d67d8581d488620eff4da9cf503313d743
Noparu <94230308+Ihsannaufal01@users.noreply.github.com>
2024-12-06 08:38:38 +0700

    Menu & Location
    hehe

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar'
+import Menu from './pages/Menu';
 import Home from './pages/home';
 
 function App() {
@@ -8,7 +9,11 @@ function App() {
   return (
     <Router>
         <Navbar/>
-        <Home/>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/menu" element={<Menu/>}/>
+        </Routes>
+        
     </Router>
   )
 }
