87430fcbcf3f300e3c2b67af94ad22d8e5d5c7dd
Noparu <94230308+Ihsannaufal01@users.noreply.github.com>
2024-12-15 20:05:44 +0700

    Home page fix
    Yea, bug at homepage, there is a little horizontal scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar'
 import Menu from './pages/Menu';
-import Location from './pages/Location';
+import Location from './components/Location';
+import HomeFirst from './components/HomeFirst';
 import Home from './pages/home';
 import Item from './pages/Item';
 import Keranjang from './pages/Keranjang';
@@ -12,18 +13,27 @@ function App() {
 
   return (
     <Router>
-        <Navbar/>
-        <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/menu" element={<Menu/>}/>
-        <Route path="/Location" element={<Location/>}/>
-        <Route path="/menu/item" element={<Item/>}/>
-        <Route path="/cart" element={<Keranjang/>}/>
-        <Route path="/login" element={<Login/>}/>
-        </Routes>
-        
+        <div className="App">
+          <Navbar/>
+          <div className="Content">
+            <Routes>
+              <Route path='/' element={
+                <div className="w-screen min-h-screen absolute overflow-hidden overflow-x-hidden top-0 left-0 right-0">
+                  <HomeFirst/>
+                  <Location/>
+                </div>
+              }/>
+              <Route path='/menu' element={<Menu/>}/>
+              <Route path='/menu/item' element={<Item/>}/>
+              <Route path='/cart' element={<Keranjang/>}/>
+              <Route path='/login' element={<Login/>}/>
+            </Routes>
+          </div>
+        </div>
     </Router>
   )
 }
 
 export default App
+
+// w-screen min-h-screen absolute top-0 left-0 right-0
